49d9639eb29131d1d65bce3a7e11a982ed281599
Kanisetty Rao <krao@opentext.com>
2019-09-10 08:54:39 +0530

    added

---
+++ Auto install scripts/artifactory.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm -P /root
+
+rpm -ivh /root/jdk-8u131-linux-x64.rpm
+
+wget https://bintray.com/jfrog/artifactory-pro-rpms/rpm -O bintray-jfrog-artifactory-pro-rpms.repo
+sudo mv bintray-jfrog-artifactory-pro-rpms.repo /etc/yum.repos.d
+yum install jfrog-artifactory-pro -y
+systemctl start artifactory.service
+systemctl status artifactory.service
---
+++ Auto install scripts/jenkins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm -P /opt
+
+rpm -ivh /root/jdk-8u131-linux-x64.rpm
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+sudo yum install jenkins -y
+sudo service jenkins start
+cat /var/lib/jenkins/secrets/initialAdminPassword
